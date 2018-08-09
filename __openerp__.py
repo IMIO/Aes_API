5d08fd3cb780b34a683bc2403a8285b6c5312beb
mcolicchia <m.colicchia@imio.be>
2018-08-09 08:36:55 +0000

    Update __openerp__.py

--- __openerp__.py
+++ __openerp__.py
@@ -3,6 +3,7 @@
     'version': '0.1',
     'depends': ['base','extraschool'],
     'author': "Imio",
+    'website': "https://www.imio.be",
     'category': 'Api',
     'description': """
     This is an API to communicate with AES through odoo's webservice. This is made to work with iA.Teleservice.
