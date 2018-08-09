42d9e03e945e4a401603695e627d1f5ee0bec346
mcolicchia <m.colicchia@imio.be>
2018-08-09 08:35:55 +0000

    Add new file

---
+++ __openerp__.py
@@ -0,0 +1,18 @@
+{
+    'name': "AES API",
+    'version': '0.1',
+    'depends': ['base','extraschool'],
+    'author': "Imio",
+    'category': 'Api',
+    'description': """
+    This is an API to communicate with AES through odoo's webservice. This is made to work with iA.Teleservice.
+    """,
+    # data files always loaded at installation
+    'data': [
+        
+    ],
+    # data files containing optionally loaded demonstration data
+    'demo': [
+        
+    ],
+}
